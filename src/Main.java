9c132fc19a29e1710725de322d3092014bf4b3b4
oAndin <00andherson@gmail.com>
2024-05-31 11:23:54 -0300

    commit bf pull

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,5 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
-        JFrame f;
-        JTextField t;
-        JButton b1,b2,b3,b4,b5,b6,b7,b8, b9, b0, bdiv, bmul,bsub,badd,bdec,beq,bdel,bclr;
-
     }
 }
